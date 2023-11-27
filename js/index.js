123ecdf67a9421219a16a1ecf2ca61dfba47dbbd
jafar2014 <itschoolxamdamov@gmail.com>
2023-11-27 11:14:08 +0500

    uyga vazifa

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var Dark_mode = document.querySelector(".dark-mod");
+var body = document.body;
+var Navbar = document.querySelector(".hh")
+var Navbar = document.querySelector(".hh")
+var Navbar_black = document.querySelector(".gg")
+var black = document.querySelector(".rr")
+
+
+Dark_mode.addEventListener("click", ()=> {
+body.classList.toggle("dark-mode")
+Navbar.classList.toggle("dark-mode")
+Navbar_black.classList.toggle("dark-mode")
+black.classList.toggle("dark-mode")
+})
